bb7918d24334dd126994d42f1a884c0175dec8a4
Shikhar K <shikharkohli@gmail.com>
2025-03-27 18:19:49 +0530

    updating env

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,8 +2,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_SUPABASE_URL: string;
-  readonly VITE_SUPABASE_ANON_KEY: string;
+  readonly VITE_SUPABASE_URL: https://ijvvdcvzgjsjivustdgs.supabase.co;
+  readonly VITE_SUPABASE_ANON_KEY: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlqdnZkY3Z6Z2pzaml2dXN0ZGdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMwNzY3ODksImV4cCI6MjA1ODY1Mjc4OX0.8RiKCEiJMIarbpiw7gA8G9Gbal1MvShm1vqCshqx-zs;
   readonly VITE_RECAPTCHA_SITE_KEY: string;
 }
 
